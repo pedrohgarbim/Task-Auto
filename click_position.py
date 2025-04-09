fc5dbe17173797a56b366c5ffc49c1d2b479e76d
Pedro H. Garbim <133874619+pedrohgarbim@users.noreply.github.com>
2025-04-09 17:23:59 -0300

    Update click_position.py

--- click_position.py
+++ click_position.py
@@ -1,7 +1,9 @@
 import time
 import pyautogui
 
-time.sleep(5)
+time.sleep(5) # para esperar para mostrar as coordenadas, assim vai dar para entrar na parte especifica 
 print(pyautogui.position())
 
 pyautogui.scroll(200)
+
+# O objetivo desse arquivo é para descobrir a posição no mouse na tela 
